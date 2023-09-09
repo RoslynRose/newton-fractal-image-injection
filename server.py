ecdc8f1e4d71210368b1617e5ee571ede1ac8ae0
RoslynRose <rosejQR@gmail.com>
2023-09-09 16:39:05 -0600

    fixed two bugs, caching bug and the function input bug

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
+
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
-from PIL import Image, ImageOps
+from PIL import Image
 import os
+import time
 
 import gpuNewt
 
@@ -14,6 +16,7 @@ PROCESSED_FOLDER = "processed_images"
 def upload_image():
     if 'image' not in request.files:
         return jsonify({"error": "No image part"}), 400
+
     image = request.files['image']
     string1 = request.form.get('string1')
     string2 = request.form.get('string2')
@@ -21,7 +24,7 @@ def upload_image():
     float2 = float(request.form.get('float2'))
     float3 = float(request.form.get('float3'))
     
-    # Print the received variables
+    # Print the received variables for debugging
     print(f"Received string1: {string1}")
     print(f"Received string2: {string2}")
     print(f"Received float1: {float1}")
@@ -32,22 +35,26 @@ def upload_image():
         return jsonify({"error": "No selected image"}), 400
     if image:
         # Ensure filename has an extension
-        filename = image.filename
-        if '.' not in filename:
+        filename, ext = os.path.splitext(image.filename)
+        if not ext:
             return jsonify({"error": "No file extension"}), 400
         
+        # Attach a Unix timestamp to the filename
+        timestamp = str(int(time.time()))
+        new_filename = f"{filename}_{timestamp}{ext}"
+        
         # Save original image
-        original_path = os.path.join(UPLOAD_FOLDER, filename)
+        original_path = os.path.join(UPLOAD_FOLDER, new_filename)
         image.save(original_path)
         
         # Open and process image to grayscale
         img = Image.open(original_path)
         fractal_generator = gpuNewt.NewtonFractalGenerator(width=800, height=800, image_path=original_path)
-        img = fractal_generator.generate_image("z*z*z-1", 1)
-        processed_path = os.path.join(PROCESSED_FOLDER, filename)
+        img = fractal_generator.generate_image(string1, 1)
+        processed_path = os.path.join(PROCESSED_FOLDER, new_filename)
         img.save(processed_path)
         
-        return jsonify({"message": "Image uploaded and processed successfully", "processed_path": f"/processed/{filename}"}), 200
+        return jsonify({"message": "Image uploaded and processed successfully", "processed_path": f"/processed/{new_filename}"}), 200
 
 @app.route('/processed/<filename>')
 def processed_image(filename):
